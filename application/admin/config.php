13cb970ab46363fb2d5244937f2e08f3390e3800
Felix <1446500327@qq.com>
2016-07-19 23:33:33 +0800

    项目代码更新

---
+++ application/admin/config.php
@@ -0,0 +1,13 @@
+<?php 
+
+return [
+	'view_replace_str' => [
+		'__IMAGES__' => '/static/admin/images',
+		'__CSS__' => '/static/admin/css',
+		'__JS__' => '/static/admin/js',
+		'__LIB_CSS__' => '/static/lib/css',
+		'__LIB_JS__' => '/static/lib/js'
+	]
+]
+
+?>
