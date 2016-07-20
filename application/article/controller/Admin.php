02ddcb56c63ce568e0716a19abb86843a7e9aacc
Felix <1446500327@qq.com>
2016-07-21 00:16:46 +0800

    文章模块

---
+++ application/article/controller/Admin.php
@@ -0,0 +1,25 @@
+<?php
+// +----------------------------------------------------------------------
+// | ehcms [ Efficient Handy Content Management System ]
+// +----------------------------------------------------------------------
+// | Copyright (c) http://ehcms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: zky < zky@ehcms.com >
+// +----------------------------------------------------------------------
+
+namespace app\article\controller;
+
+use think\Controller;
+
+/**
+ * 文章模块后台控制器
+ */
+class Admin extends Controller{
+	
+	public function get(){
+		
+	}
+	
+}
