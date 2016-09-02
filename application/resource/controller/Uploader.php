f4c90a2019ba633e8735296fa3586a986225dc52
Felix <1446500327@qq.com>
2016-09-02 23:53:34 +0800

    提交

--- application/resource/controller/Uploader.php
+++ application/resource/controller/Uploader.php
@@ -16,6 +16,11 @@ namespace app\resource\controller;
  *
  */
 class Uploader{
+	//图片类型的编号及允许范围
+	const TYPE_IMAGE_NUMBER = 1;
+	
+
+	private $typeImageExtension = ['jpg', 'jpeg', 'gif', 'png', 'bmp'];
 	
 	/**
 	 * 文件接收器，所有资源的上传入口。
@@ -24,28 +29,28 @@ class Uploader{
 		//获取文件，目前仅支持单文件，后期再加多文件的处理
 		$file = request()->file('file');
 		
+		unlink('./' . 'uploader/article/20160902/b6bfbf0e93f5e05ffaad3e927baa9805.png');
+		
+		//print_r($file);
+		die;
+		
 		//组合文件上传目录
 		$post = input('post.');
-		$base = 'uploader/';
-		
-		if (!empty($post['uploaderPath'])){
-			$path = $base . $post['uploaderPath'];
-		}elseif (!empty($post['uploaderType'])){
-			$path = $base . $post['uploaderType'];
-		}else{
-			$path = $base . 'default';
-		}
+		$path = 'uploader/';
 		
 		$info = $file->move($path);
 		
-		echo $info->getExtension()."\n";
-		echo $info->getBasename()."\n";
-		echo $info->getFileInfo()."\n";
-		echo $info->getFilename ()."\n";
-		echo $info->getLinkTarget()."\n";
-		echo $info->getPath()."\n";
-		echo $info->getPathInfo()."\n";
-		echo $info->getPathname()."\n";
-		echo $info->getRealPath();
+		$data['extension'] = $info->getExtension();
+		$data['path'] = $info->getPathname();
+		
+		//db('resource_transfer')->insert($data)
+	}
+	
+	private function checkExtension($extension){
+		if (in_array($extension, $this->$typeImageExtension)){
+			return self::TYPE_IMAGE_NUMBER;
+		}
+		
+		return false;
 	}
 }
