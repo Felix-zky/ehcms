954318317dafb4419000c22a884c416d5d916e2a
Felix <1446500327@qq.com>
2017-05-29 18:06:46 +0800

    日常提交

--- application/admin/controller/Setting.php
+++ application/admin/controller/Setting.php
@@ -3,4 +3,11 @@ namespace app\admin\controller;
 
 class Setting extends Init{
 	
+	public function system(){
+		return $this->fetch();
+	}
+	
+	public function personal(){
+		return $this->fetch();
+	}
 }
