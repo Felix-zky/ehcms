28c9ded2ecc04d27350f5dcf0f3b1d5bec3b9510
Felix <1446500327@qq.com>
2017-07-02 00:08:30 +0800

    日常提交

--- application/common.php
+++ application/common.php
@@ -1,22 +1,28 @@
 <?php
 use think\Route;
 
-//注册后台普通路由
-Route::rule('admin/article/resource', 'admin/Article/resource');
-Route::post('admin/Member/list', 'admin/Member/getMemberList');
-Route::rule('admin/Resource/uploader/[:parentGroupID]/[:childrenGroupID]', 'admin/Resource/uploader', '*', [], ['parentGroupID' => '\d+|all', 'childrenGroupID' => '\d+']);
-Route::rule('admin/document/cover', 'admin/Document/uploaderCover');
-Route::rule('admin/article_category/parent_id/:parent_id', 'admin/ArticleCategory/index', 'get', [], ['parent' => '\d+']);
-Route::rule('admin/document_category/parent_id/:parent_id', 'admin/DocumentCategory/index', 'get', [], ['parent' => '\d+']);
-Route::rule('admin/goods_category/parent_id/:parent_id', 'admin/GoodsCategory/index', 'get', [], ['parent' => '\d+']);
-Route::get('admin/article/getcategory', 'admin/Article/getCategory');
-Route::get('admin/document/getcategory', 'admin/Document/getCategory');
-Route::get('admin/goods/getcategory', 'admin/Goods/getCategory');
-
-//注册后台资源路由
-Route::resource('admin/article', 'admin/Article');
-Route::resource('admin/article_category', 'admin/ArticleCategory');
-Route::resource('admin/document', 'admin/Document');
-Route::resource('admin/document_category', 'admin/DocumentCategory');
-Route::resource('admin/goods', 'admin/Goods');
-Route::resource('admin/goods_category', 'admin/GoodsCategory');
+Route::domain('admin', function(){
+	Route::get('/', 'admin/Index/index');
+	
+	//注册后台普通路由
+	Route::rule('article/resource', 'admin/Article/resource');
+	Route::post('Member/list', 'admin/Member/getMemberList');
+	Route::rule('Resource/uploader/[:parentGroupID]/[:childrenGroupID]', 'admin/Resource/uploader', '*', [], ['parentGroupID' => '\d+|all', 'childrenGroupID' => '\d+']);
+	Route::rule('document/cover', 'admin/Document/uploaderCover');
+	Route::rule('article_category/parent_id/:parent_id', 'admin/ArticleCategory/index', 'get', [], ['parent' => '\d+']);
+	Route::rule('document_category/parent_id/:parent_id', 'admin/DocumentCategory/index', 'get', [], ['parent' => '\d+']);
+	Route::rule('goods_category/parent_id/:parent_id', 'admin/GoodsCategory/index', 'get', [], ['parent' => '\d+']);
+	Route::get('article/getcategory', 'admin/Article/getCategory');
+	Route::get('document/getcategory', 'admin/Document/getCategory');
+	Route::get('goods/getcategory', 'admin/Goods/getCategory');
+	
+	//注册后台资源路由
+	Route::resource('article', 'admin/Article');
+	Route::resource('article_category', 'admin/ArticleCategory');
+	Route::resource('document', 'admin/Document');
+	Route::resource('document_category', 'admin/DocumentCategory');
+	Route::resource('goods', 'admin/Goods');
+	Route::resource('goods_category', 'admin/GoodsCategory');
+	
+	Route::rule(':controller/:action', 'admin/:controller/:action');
+});
--- application/config.php
+++ application/config.php
@@ -93,7 +93,7 @@ return [
     // 是否强制使用路由
     'url_route_must'         => false,
     // 域名部署
-    'url_domain_deploy'      => false,
+    'url_domain_deploy'      => true,
     // 域名根，如.thinkphp.cn
     'url_domain_root'        => '',
     // 是否自动转换URL中的控制器和操作名
