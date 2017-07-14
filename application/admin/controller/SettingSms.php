b1093dfe60e0cd01d183ee5b05f3bc146a3bd51c
Felix <1446500327@qq.com>
2017-07-14 18:35:03 +0800

    日常提交

--- application/admin/controller/SettingSms.php
+++ application/admin/controller/SettingSms.php
@@ -4,25 +4,28 @@ namespace app\admin\controller;
 class SettingSms extends Init{
 	
 	public function index(){
+		$sms = db('admin_setting')->where('key', 'aliyun_sms')->find();
+		
+		$sms['value'] = unserialize($sms['value']);
+		
+		$this->assign('sms', $sms['value']);
 		return $this->fetch();
 	}
 	
 	public function update(){
 		if (request()->isPut()){
-			print_r(input('param.'));
-			die;
-				
 			$data = [
 				'name' => '阿里云短信接口配置',
 				'key' => 'aliyun_sms',
-				'data' => serialize(input('param.'))
+				'value' => serialize(input('param.'))
 			];
 				
 			$result = db('admin_setting')->where('key', 'aliyun_sms')->update($data);
 				
 			if ($result != 1){
-				echo 123;
-				die;
+				$this->errorResult('更新失败');
+			}else{
+				$this->successResult('更新成功');
 			}
 		}
 	}
