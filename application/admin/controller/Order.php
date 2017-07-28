10135ffbb05a32a9f04f2257a652c4f1e2a5554e
Felix <1446500327@qq.com>
2017-07-28 12:08:57 +0800

    日常提交

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -3,4 +3,11 @@ namespace app\admin\controller;
 
 class Order extends Init{
 	
+	public function index(){
+		$order = model('order')->order('id', 'desc')->paginate(10);
+	
+		$this->assign('order', $order);
+		return $this->fetch();
+	}
+	
 }
