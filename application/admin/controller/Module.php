f066fa051c11a855917ebe61a91e6b95cd892191
Felix <1446500327@qq.com>
2017-08-01 18:15:13 +0800

    日常提交

--- application/admin/controller/Module.php
+++ application/admin/controller/Module.php
@@ -2,11 +2,14 @@
 namespace app\admin\controller;
 
 class Module extends Init{
-	public function __construct(){
-		parent::__construct();
-	}
-	
 	public function index(){
+		$module = db('admin_module')->select();
+		$menus = db('admin_menu')->field('name, module_id')->where('parent_id', 0)->group('module_id')->select();
+		
+		var_dump($menus);
+		die;
+		
+		$this->assign('module', $module);
 		return $this->fetch();
 	}
 }
