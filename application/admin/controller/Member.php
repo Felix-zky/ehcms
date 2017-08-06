12c4531730e78e614b25aada5d3b751cace98b5a
Felix <1446500327@qq.com>
2017-08-06 17:45:02 +0800

    日常提交

--- application/admin/controller/Member.php
+++ application/admin/controller/Member.php
@@ -1,4 +1,14 @@
 <?php
+// +----------------------------------------------------------------------
+// | ehcms [ Efficient Handy Content Management System ]
+// +----------------------------------------------------------------------
+// | Copyright (c) http://ehcms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: zky < zky@ehcms.com >
+// +----------------------------------------------------------------------
+
 namespace app\admin\controller;
 
 class Member extends Init{
