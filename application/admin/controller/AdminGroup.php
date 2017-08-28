d3eb139c96e2a2d70d305952a646ee382e6fb7eb
Felix <1446500327@qq.com>
2017-08-28 18:11:36 +0800

    日常提交

--- application/admin/controller/AdminGroup.php
+++ application/admin/controller/AdminGroup.php
@@ -29,11 +29,35 @@ class AdminGroup extends Init{
 	public function getPermission(){
 		$type = input('type');
 		$group = input('group');
+		$selected = [];
 		
-		if (type == 'edit' && !emtpy($group)){
+		$module = db('admin_module')->select();
+		$permissionResult = db('admin_permission')->field('p.id, p.group_id, p.name, p.key, pg.name as group_name, pg.module_id')->alias('p')->join('admin_permission_group pg', 'pg.id = p.group_id')->select();
 			
-		}else{
+		$permission = [];
+		foreach ($permissionResult as $v){
+			if (empty($permission[$v['module_id']][$v['group_id']])){
+				$permission[$v['module_id']][$v['group_id']] = [
+						'name' => $v['group_name'],
+						'permission' => []
+				];
+			}
+		
+			$permission[$v['module_id']][$v['group_id']]['permission'][$v['id']] = [
+					'name' => $v['name'],
+					'key' => $v['key']
+			];
+		}
+		
+		if ($type == 'edit' && !emtpy($group)){
 			
 		}
+
+		$data = [
+			'module' => $module,
+			'permission' => $permission
+		];
+		
+		$this->successResult($data);
 	}
 }
