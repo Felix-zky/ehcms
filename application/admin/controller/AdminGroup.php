c103c3b418169b4db430426cc88d564ff358b362
Felix <1446500327@qq.com>
2018-01-28 17:33:37 +0800

    日常提交

--- application/admin/controller/AdminGroup.php
+++ application/admin/controller/AdminGroup.php
@@ -33,41 +33,40 @@ class AdminGroup extends Init{
 	}
 
 	public function edit($id){
+	    $this->assign('groupID', $id);
         $this->assign('actionSign', 'editor');
 	    return $this->fetch('editor');
     }
 
 	public function getPermission(){
-		$type = input('type');
-		$group = input('group');
-		$selected = [];
-		
+	    $group = input('group');
+
+        if (!empty($group)){
+
+        }
+
 		$module = db('admin_module')->select();
 		$permissionResult = db('admin_permission')->field('p.id, p.group_id, p.name, p.key, pg.name as group_name, pg.module_id')->alias('p')->join('admin_permission_group pg', 'pg.id = p.group_id')->select();
 			
 		$permission = [];
 		foreach ($permissionResult as $v){
 			if (empty($permission[$v['module_id']][$v['group_id']])){
-				$permission[$v['module_id']][$v['group_id']] = [
-						'name' => $v['group_name'],
-						'permission' => []
-				];
+                $permission[$v['module_id']][$v['group_id']] = [
+                    'name' => $v['group_name'],
+                    'permission' => []
+                ];
 			}
-		
-			$permission[$v['module_id']][$v['group_id']]['permission'][$v['id']] = [
-					'name' => $v['name'],
-					'key' => $v['key']
-			];
-		}
-		
-		if ($type == 'edit' && !emtpy($group)){
-			
+
+            $permission[$v['module_id']][$v['group_id']]['permission'][$v['id']] = [
+                'name' => $v['name'],
+                'key' => $v['key']
+            ];
 		}
 
-		$data = [
-			'module' => $module,
-			'permission' => $permission
-		];
+        $data = [
+            'module' => $module,
+            'permission' => $permission
+        ];
 		
 		$this->successResult($data);
 	}
